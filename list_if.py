d332011629ce0e3096d80020c5912ac32b309c10
servantes2215 <s.batyaev1995@mail.ru>
2021-03-03 00:37:47 +0400

    z1_if

---
+++ list_if.py
@@ -0,0 +1,7 @@
+request_topings = ['mushrooms', 'cherry', 'pepers', 'cheeze']
+for request_toping in request_topings:
+    if request_toping == 'pepers':
+        print("Sorry but not now")
+    else:
+        print("Adding" + request_toping)
+print("Making your pizza")
