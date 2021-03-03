4a9177ae0321491cf087b031894226c7210973a9
servantes2215 <s.batyaev1995@mail.ru>
2021-03-03 23:51:10 +0400

    5.8_complete

--- 5.8.users.py
+++ 5.8.users.py
@@ -1,1 +1,6 @@
 users = ['admin', 'user', 'abcd', '1234', 'super_user']
+for admin in users:
+    if admin == 'admin':
+        print("Hello " + admin.title())
+    else:
+        print("user not found")
